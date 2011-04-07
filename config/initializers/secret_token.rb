93b330b5f2be898df4fb7fb1b9a6cc248c7e8b6d
Pascal Widdershoven <pascal@pwiddershoven.nl>
2011-04-07 14:32:48 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trendingtopic::Application.config.secret_token = '074a271333dbf0db381e8c34a99b6ec601936086124dcb704911dfacbd52d9c758cd068afb7ff7c9f0908bd9c2f1911848915b4862c0c3ac4b748990a7593764'
